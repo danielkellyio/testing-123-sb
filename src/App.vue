75e1fb909f9122a149c5c2f80f1822c8ddc16fca
Daniel Kelly <me@danielkelly.io>
2024-02-08 13:03:00 -0600

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const test = ref(true)
+test.value = false
+</script>
 <template>
   <div class="page">
     <h1 class="text-3xl">Hello world</h1>
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+import { ref } from 'vue'
+const test = ref(true)
+test.value = false
